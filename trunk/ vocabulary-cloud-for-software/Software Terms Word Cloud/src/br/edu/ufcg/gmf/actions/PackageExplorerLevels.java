49f080fe9251ec483c8574b7d82612db49a41d5b
ccc.samuel <ccc.samuel@c4a96177-931e-0d21-0019-9858d673621a>
2010-07-15 13:52:57 +0000

    Defines the level of importance of java elements in the hierarchy.

---
+++ trunk/ vocabulary-cloud-for-software/Software Terms Word Cloud/src/br/edu/ufcg/gmf/actions/PackageExplorerLevels.java
@@ -0,0 +1,6 @@
+package br.edu.ufcg.gmf.actions;
+
+public enum PackageExplorerLevels {
+    NONE, SOURCE_METHOD, SOURCE_TYPE, I_PACKAGE_FRAGMENT, I_PACKAGE_FRAGMENT_ROOT, I_JAVA_PROJECT
+    // SOURCE_TYPE represents I_COMPILATION_UNIT
+}
