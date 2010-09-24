757b8f61bf3f410a1534767f01ec84e3e58a9c2d
gugajansen <gugajansen@c4a96177-931e-0d21-0019-9858d673621a>
2010-09-24 12:11:18 +0000

    inclusão da biblioteca de visualização em círculos (prefuse) + leitura do vocabulário a partir da AST

--- trunk/ vocabulary-cloud-for-software/Software Terms Word Cloud/src/br/edu/ufcg/gmf/actions/TermsCloudGenerator.java
+++ trunk/ vocabulary-cloud-for-software/Software Terms Word Cloud/src/br/edu/ufcg/gmf/actions/TermsCloudGenerator.java
@@ -1,23 +1,16 @@
 package br.edu.ufcg.gmf.actions;
 
 import java.io.IOException;
-import java.util.TreeMap;
-import java.util.Map.Entry;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.ICompilationUnit;
-import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
-import org.eclipse.jdt.core.IType;
-import org.eclipse.jdt.core.JavaModelException;
-import org.eclipse.jdt.internal.core.JarPackageFragmentRoot;
-import org.eclipse.jdt.internal.core.SourceMethod;
 import org.eclipse.jdt.internal.core.SourceType;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
@@ -29,19 +22,20 @@ import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
+import br.edu.ufcg.gmf.astexplorer.ASTVocabularyExtractor;
+import br.edu.ufcg.gmf.astexplorer.VocabularyConverter;
 import br.edu.ufcg.gmf.imageRetrieval.WordCloudViewer;
 
 @SuppressWarnings("restriction")
 public class TermsCloudGenerator implements IWorkbenchWindowActionDelegate, IObjectActionDelegate {
+	
 	private IWorkbenchWindow window;
 	private ISelection userSelection;
 	private PackageExplorerHierarchy userSelectionType;
-	private TreeMap<String, Integer> wordCloudMap = new TreeMap<String, Integer>();
+	
+	private ASTVocabularyExtractor vocExtractor;
+	private Map<String, Double> wordCloudMap = new HashMap<String, Double>();
 	private String wordCloudString;
-	private static boolean includeProject, includePackagesFolders, includePackages, includeClass, includeSuperClass, includeInterfaces, includeMethods, includeParameters;
-
-	public TermsCloudGenerator() {
-	}
 
 	public void run(IAction action) {
 		wordCloudString = "";
@@ -58,10 +52,8 @@ public class TermsCloudGenerator implements IWorkbenchWindowActionDelegate, IObj
 			}
 		} catch (IOException Error) {
 			openMessageDialog("You probably do not have internet connection. It's necessary.");
-			//Error.printStackTrace();
 		} catch (Exception Error) {
-			openMessageDialog("Sorry! An exception occurred, could not generate the Word Cloud.");
-			//Error.printStackTrace();
+			openMessageDialog("Sorry! An exception occurred, could not generate the Word Cloud. \n" + Error.getMessage());
 		} 
 	}
 	
@@ -69,6 +61,7 @@ public class TermsCloudGenerator implements IWorkbenchWindowActionDelegate, IObj
 		Preferences.run(userSelectionType);		
 	}
 
+	// extracts user selection
 	private void identifyUserSelectionType() {
 		if (userSelection instanceof TreeSelection) {
 			Object firstElement = ((TreeSelection) userSelection).getFirstElement();
@@ -86,8 +79,6 @@ public class TermsCloudGenerator implements IWorkbenchWindowActionDelegate, IObj
 					userSelectionType = PackageExplorerHierarchy.I_COMPILATION_UNIT;
 				} else if(firstElement instanceof SourceType) {
 					userSelectionType = PackageExplorerHierarchy.SOURCE_TYPE;
-				} else if (firstElement instanceof SourceMethod) { // Method in any Class
-					userSelectionType = PackageExplorerHierarchy.SOURCE_METHOD;
 				} else {
 					openMessageDialog("Please, select something in Package Explorer.");
 				}
@@ -95,6 +86,48 @@ public class TermsCloudGenerator implements IWorkbenchWindowActionDelegate, IObj
 		} else if (userSelection instanceof TextSelection) {
 			userSelectionType = PackageExplorerHierarchy.SELECTION;
 		} 
+	}
+	
+	// load terms from user's selected elements
+	private void loadWordsToMap() throws Exception {
+		
+		this.vocExtractor = new ASTVocabularyExtractor();
+		this.vocExtractor.setProperties(includeProject, includePackagesFolders, includePackages, includeClass, 
+				includeSuperClass, includeInterfaces, includeMethods, includeParameters);
+		
+		if (!(userSelectionType.equals(PackageExplorerHierarchy.SELECTION))) {
+			Object selectedElement = ((TreeSelection) userSelection).getFirstElement();
+			
+			if (userSelectionType.equals(PackageExplorerHierarchy.I_JAVA_PROJECT)) {
+				this.vocExtractor.extractProjectInfo((IJavaProject) selectedElement);
+			} else if (userSelectionType.equals(PackageExplorerHierarchy.I_PACKAGE_FRAGMENT_ROOT)) {
+				this.vocExtractor.extractProjectFolderInfo(0, (IPackageFragmentRoot) selectedElement);
+			} else if (userSelectionType.equals(PackageExplorerHierarchy.I_PACKAGE_FRAGMENT)) {
+				this.vocExtractor.extractPackageInfo(0, (IPackageFragment) selectedElement);
+			} else if (userSelectionType.equals(PackageExplorerHierarchy.I_COMPILATION_UNIT)) {
+				this.vocExtractor.extractCompilationUnitInfo((ICompilationUnit) selectedElement);
+			} else if (userSelectionType.equals(PackageExplorerHierarchy.SOURCE_TYPE)) {
+				this.vocExtractor.extractTypeInfo(0, (SourceType) selectedElement);
+			} 
+		} else {
+			TextSelection selectionText = (TextSelection) userSelection;
+			this.wordCloudString = selectionText.getText();
+		}
+	}
+	
+	private void loadMapToWCString() {
+		
+		if (this.vocExtractor != null) {
+			
+			VocabularyConverter vocConverter = new VocabularyConverter(this.vocExtractor.getVocabulary());
+			this.wordCloudMap = vocConverter.convertToFrequencyMap();
+			
+			for (String term : wordCloudMap.keySet()) {
+				long frequency = Math.round(wordCloudMap.get(term));
+				for (int i = 0; i < frequency; i++)
+					wordCloudString += term + " ";
+			}
+		}
 	}
 	
 	private void showWCImageInBrowser() throws IOException {
@@ -114,140 +147,8 @@ public class TermsCloudGenerator implements IWorkbenchWindowActionDelegate, IObj
 		}
 	}
 	
-	private void loadMapToWCString() {
-		//while (!(wordCloudMap.isEmpty())) {
-		//	wordCloudString += getMostImportantWordInWCMap() + " ";
-		//}
-		for (Entry<String, Integer> entry : wordCloudMap.entrySet()) {
-			for (int i = 0; i < entry.getValue(); i++) {
-				wordCloudString += entry.getKey() + " ";
-			}
-		} 
-	}
-	
-	private void loadWordsToMap() throws CoreException {
-		if (!(userSelectionType.equals(PackageExplorerHierarchy.SELECTION))) {
-			Object firstElement = ((TreeSelection) userSelection).getFirstElement();
-			
-			if (userSelectionType.equals(PackageExplorerHierarchy.I_JAVA_PROJECT)) {
-				loadJavaProjectInformations((IJavaProject) firstElement);
-			} else if (userSelectionType.equals(PackageExplorerHierarchy.I_PACKAGE_FRAGMENT_ROOT)) {
-				loadPackageFragmentRootInformations((IPackageFragmentRoot) firstElement);
-			} else if (userSelectionType.equals(PackageExplorerHierarchy.I_PACKAGE_FRAGMENT)) {
-				loadPackageFragmentInformations((IPackageFragment) firstElement);
-			} else if (userSelectionType.equals(PackageExplorerHierarchy.I_COMPILATION_UNIT)) {
-				loadCompilationUnitInformations((ICompilationUnit) firstElement);
-			} else if (userSelectionType.equals(PackageExplorerHierarchy.SOURCE_TYPE)) {
-				loadSourceTypeInformations((SourceType) firstElement);
-			} else if (userSelectionType.equals(PackageExplorerHierarchy.SOURCE_METHOD)) {
-				loadSourceMethodsInformations((SourceMethod) firstElement);
-			}
-		} else {
-			TextSelection selectionText = (TextSelection) userSelection;
-			wordCloudString = selectionText.getText();
-		}
-	}
-	
-	private void loadJavaProjectInformations(IJavaProject project) throws JavaModelException {
-		if (includeProject) {
-			addInWCMap(project.getElementName(), PackageExplorerLevels.I_JAVA_PROJECT.ordinal());
-		} for (IPackageFragmentRoot pfr: project.getPackageFragmentRoots()) {
-			if (!(pfr instanceof JarPackageFragmentRoot)) {
-				loadPackageFragmentRootInformations(pfr);
-			}
-		}
-	}
-	
-	private void loadPackageFragmentRootInformations(IPackageFragmentRoot packageFragmentRoot) throws JavaModelException {
-		if (includePackagesFolders) {
-			addInWCMap(packageFragmentRoot.getElementName(), PackageExplorerLevels.I_PACKAGE_FRAGMENT_ROOT.ordinal());
-		} for (IJavaElement javaElement : packageFragmentRoot.getChildren()) {
-			if (javaElement instanceof IPackageFragment) {
-				loadPackageFragmentInformations((IPackageFragment) javaElement);
-			}
-		}
-	}
-	
-	private void loadPackageFragmentInformations(IPackageFragment pack) throws JavaModelException {
-		if (includePackages) {
-			addInWCMap(pack.getElementName(), PackageExplorerLevels.I_PACKAGE_FRAGMENT.ordinal());	
-		} for (ICompilationUnit compilation : pack.getCompilationUnits()) {
-			for (IType type : compilation.getTypes()) {
-				if (type instanceof SourceType) {
-					SourceType sourceTypeClass = (SourceType) type;
-					loadSourceTypeInformations(sourceTypeClass);
-				}
-			}
-		}
-	}
-	
-	private void loadCompilationUnitInformations(ICompilationUnit clasS) throws JavaModelException {
-		for (IType type : clasS.getTypes()) {
-			if (type instanceof SourceType) {
-				SourceType sourceType = (SourceType) type;
-				loadSourceTypeInformations(sourceType);
-			}
-		}
-	}
-	
-	private void loadSourceTypeInformations(SourceType sourceTypeClass) throws JavaModelException {
-		if (includeClass) {
-			addInWCMap(sourceTypeClass.getElementName(), PackageExplorerLevels.SOURCE_TYPE.ordinal());
-		} if (includeSuperClass && sourceTypeClass.getSuperclassName() != null) {
-			addInWCMap(sourceTypeClass.getSuperclassName());
-		} if (includeInterfaces) {
-			for (String interfacE : sourceTypeClass.getSuperInterfaceNames()) {
-				addInWCMap(interfacE);
-			}
-		} for (IMethod method : sourceTypeClass.getMethods()) {
-				loadSourceMethodsInformations(method);
-		}
-	}
-	
-	private void loadSourceMethodsInformations(IMethod method) throws JavaModelException {
-		if (includeMethods) {
-			addInWCMap(method.getElementName(), PackageExplorerLevels.SOURCE_METHOD.ordinal());
-		} if (includeParameters) {
-			for (String parameter : method.getParameterNames()) {
-				addInWCMap(parameter);
-			}
-		}
-	}
-	
-	private void addInWCMap(String word) {
-		if (!(wordCloudMap.containsKey(word))) {
-			wordCloudMap.put(word, 0);
-		} wordCloudMap.put(word, wordCloudMap.get(word) + 1);
-	}
-	
-	private void addInWCMap(String word, int amount) {
-		for (int iteration = 0; iteration < amount; iteration++) {
-			if (iteration == 0 && !(wordCloudMap.containsKey(word))) {
-				wordCloudMap.put(word, 1);
-				continue;
-			} wordCloudMap.put(word, wordCloudMap.get(word) + 1);
-		}
-	}
-	
-	private String getMostImportantWordInWCMap() {
-		String wordOfMoreFrequently = null;
-		int moreFrequently = (Integer) wordCloudMap.values().toArray()[0];
-		
-		for (int frequently : wordCloudMap.values()) {
-			if (frequently > moreFrequently) {
-				moreFrequently = frequently;
-			}
-		}
-		
-		for (Entry<String, Integer> entry : wordCloudMap.entrySet()) {
-			if (entry.getValue() == moreFrequently) {
-				wordOfMoreFrequently = entry.getKey();
-				wordCloudMap.remove(wordOfMoreFrequently);
-				break;
-			}
-		}
-		return wordOfMoreFrequently;
-	}
+	private static boolean includeProject, includePackagesFolders, includePackages, includeClass, 
+		includeSuperClass, includeInterfaces, includeMethods, includeParameters;
 	
 	public static void setIncludeProject(boolean newValue) {
 		includeProject = newValue;
@@ -300,7 +201,5 @@ public class TermsCloudGenerator implements IWorkbenchWindowActionDelegate, IObj
 	}
 
 	@Override
-	public void setActivePart(IAction arg0, IWorkbenchPart arg1) {
-	
-	}
+	public void setActivePart(IAction arg0, IWorkbenchPart arg1) {}
 }
